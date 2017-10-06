c47cedc6b03f6fcb3ca6c4afc15ff677b5203c34
Thomas Moore <tcm0116@gmail.com>
2017-10-05 22:06:11 -0500

    HICTOP 3DP-18 (E1) Config

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -105,15 +105,15 @@
 #define Z_ENABLE_PIN       62
 #define Z_CS_PIN           40
 
-#define E0_STEP_PIN        26
-#define E0_DIR_PIN         28
-#define E0_ENABLE_PIN      24
-#define E0_CS_PIN          42
+#define E0_STEP_PIN        36
+#define E0_DIR_PIN         34
+#define E0_ENABLE_PIN      30
+#define E0_CS_PIN          44
 
-#define E1_STEP_PIN        36
-#define E1_DIR_PIN         34
-#define E1_ENABLE_PIN      30
-#define E1_CS_PIN          44
+#define E1_STEP_PIN        26
+#define E1_DIR_PIN         28
+#define E1_ENABLE_PIN      24
+#define E1_CS_PIN          42
 
 //
 // Temperature Sensors
