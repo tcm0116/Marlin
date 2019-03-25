500da36622d11426c65f80332dba637dc23dd59c
Thomas Moore <tcm0116@gmail.com>
2019-03-24 22:16:35 -0400

    Update UBL mesh inset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1325,7 +1325,7 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 10             // Set Mesh bounds as an inset region of the bed
+  #define MESH_INSET 0              // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 5       // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
